1ca20f53fb107cd29cbbd85753d7ca0b86e6b7b3
jiqb <gthbji@ml1.net>
2018-06-16 21:09:10 +0100

    Add git/commit.go for comitting staged changes

---
+++ git/commit.go
@@ -0,0 +1,58 @@
+package git
+
+import (
+	"os/exec"
+	"strings"
+)
+
+type CommitOpts struct {
+	Messages []string
+	Project  string
+}
+
+func hasStagedChanges(project string) (bool, error) {
+	command := exec.Command("git", "diff", "--cached")
+	if project != "" {
+		command.Dir = project
+	}
+
+	combinedOutput, err := command.CombinedOutput()
+	if err != nil {
+		return false, err
+	}
+
+	trimmed := strings.TrimSpace(string(combinedOutput))
+
+	return len(trimmed) > 0, nil
+}
+
+func Commit(opts CommitOpts) (string, error) {
+	changes, err := hasStagedChanges(opts.Project)
+	if err != nil {
+		return "", err
+	}
+
+	if !changes {
+		return "no staged changes to commit", err
+	}
+
+	var args []string
+	args = append(args, "commit")
+
+	for _, message := range opts.Messages {
+		args = append(args, "--message")
+		args = append(args, message)
+	}
+
+	command := exec.Command("git", args...)
+	if opts.Project != "" {
+		command.Dir = opts.Project
+	}
+
+	combinedOutput, err := command.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(string(combinedOutput)), nil
+}
