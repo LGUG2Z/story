b0a5082d2395f3d2b18db318bb175c9112e1cb74
jiqb <gthbji@ml1.net>
2018-06-17 14:18:56 +0100

    Add basic test cases for add command

--- cli/app_test.go
+++ cli/app_test.go
@@ -3,11 +3,16 @@ package cli_test
 import (
 	"os"
 
+	"encoding/json"
+	"os/exec"
+
 	"github.com/LGUG2Z/story/cli"
 	"github.com/LGUG2Z/story/git"
 	"github.com/LGUG2Z/story/manifest"
+	"github.com/LGUG2Z/story/node"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/spf13/afero"
 )
 
 var _ = Describe("App", func() {
@@ -235,5 +240,69 @@ var _ = Describe("App", func() {
 			Expect(err).To(HaveOccurred())
 			Expect(err).To(Equal(cli.ErrNotWorkingOnAStory))
 		})
+	})
+
+	Describe("Add", func() {
+		It("Should add a project to a story", func() {
+			// Given an initialised metarepo with projects and a story
+			Expect(fs.MkdirAll("one", os.FileMode(0700))).To(Succeed())
+			p := node.PackageJSON{}
+			b, err := json.Marshal(p)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(afero.WriteFile(fs, "one/package.json", b, os.FileMode(0666))).To(Succeed())
+
+			command := exec.Command("git", "init")
+			command.Dir = "one"
+			_, err = command.CombinedOutput()
+			Expect(err).NotTo(HaveOccurred())
+
+			_, err = git.Add(git.AddOpts{Project: "one", Files: []string{"package.json"}})
+			Expect(err).NotTo(HaveOccurred())
+
+			_, err = git.Commit(git.CommitOpts{Project: "one", Messages: []string{"initial commit"}})
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(cli.App().Run([]string{"story", "create", "test-story"})).To(Succeed())
+
+			// When I add a project to the story
+			Expect(cli.App().Run([]string{"story", "add", "one"})).To(Succeed())
+
+			// Then the project is in the story meta file
+			s, err := manifest.LoadStory(fs)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(s.Projects).To(HaveKeyWithValue("one", "git@github.com:test-org/one.git"))
+
+			// And the project is on the story branch
+			branch, err := git.GetCurrentBranch(fs, "one")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(branch).To(Equal("test-story"))
+
+			// And artifacts are updated
+			Expect(s.Artifacts).To(HaveKeyWithValue("one", true))
+		})
+
+		It("Should return an error if no arguments are given", func() {
+			// Given an initialised metarepo with a story
+			Expect(cli.App().Run([]string{"story", "create", "test-story"})).To(Succeed())
+
+			// When I try to add a project
+			err := cli.App().Run([]string{"story", "add"})
+
+			// Then it returns an error
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(Equal(cli.ErrCommandRequiresAnArgument))
+		})
+
+		It("Should return an error if not working on a story", func() {
+			// Given an initialised metarepo not on a story
+
+			// When I try to list projects,
+			err := cli.App().Run([]string{"story", "add", "one"})
+
+			// Then it returns an error
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(Equal(cli.ErrNotWorkingOnAStory))
+		})
+
 	})
 })
