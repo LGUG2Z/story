2283e1d71b6a059a08a9e016c5ff93052e76a100
jiqb <gthbji@ml1.net>
2019-05-18 11:37:36 -0700

    Add --github squash and merge strategy
    Extract out some GitHub API utils
    
    Stop reading the response from PR calls
    
    Comment flows and improve error handling in pr and merge commands
    
    Add --story-branch flag to allow pushing from branch when using --from-manifest (ie. after running prepare and before running merge --github)

--- cli/errors.go
+++ cli/errors.go
@@ -8,3 +8,13 @@ var ErrAlreadyWorkingOnAStory = fmt.Errorf("already working on a story")
 var ErrCommandRequiresAnArgument = fmt.Errorf("this command requires an argument")
 var ErrCommandTakesNoArguments = fmt.Errorf("this command takes no arguments")
 var ErrNotWorkingOnAStory = fmt.Errorf("not working on a story")
+var ErrGitHubAPITokenRequired = fmt.Errorf("an API token for GitHub is required, either using --github-api-token or $GITHUB_API_TOKEN")
+var ErrIssueURLRequired = fmt.Errorf("an issue URL is required")
+
+func ErrCouldNotFindOpenPullRequest(story string) error {
+	return fmt.Errorf("could not find an open pull request for %s", story)
+}
+
+func ErrCouldNotFindClosedPullRequest(story string) error {
+	return fmt.Errorf("could not find a closed pull request for %s", story)
+}
--- cli/merge.go
+++ cli/merge.go
@@ -1,11 +1,15 @@
 package cli
 
 import (
+	"context"
 	"fmt"
+	"net/http"
+	"time"
 
 	"github.com/LGUG2Z/story/git"
 	"github.com/LGUG2Z/story/manifest"
 	"github.com/fatih/color"
+	"github.com/google/go-github/github"
 	"github.com/spf13/afero"
 	"github.com/urfave/cli"
 )
@@ -14,6 +18,10 @@ func MergeCmd(fs afero.Fs) cli.Command {
 	return cli.Command{
 		Name:  "merge",
 		Usage: "Merges prepared code to master branches across the current story",
+		Flags: []cli.Flag{
+			cli.BoolFlag{Name: "github", Usage: "Use the GitHub squash and merge implementation via API"},
+			cli.StringFlag{Name: "github-api-token", EnvVar: "GITHUB_API_TOKEN", Usage: "GitHub API token to authenticate with"},
+		},
 		Action: cli.ActionFunc(func(c *cli.Context) error {
 			if !isStory {
 				return ErrNotWorkingOnAStory
@@ -34,6 +42,75 @@ func MergeCmd(fs afero.Fs) cli.Command {
 			}
 
 			messages := []string{fmt.Sprintf("[story merge] Merge branch '%s'", story.Name)}
+
+			if c.Bool("github") {
+				if len(c.String("github-api-token")) == 0 {
+					return ErrGitHubAPITokenRequired
+				}
+
+				ctx := context.Background()
+				client := getGitHubClient(ctx, c.String("github-api-token"))
+
+				story.Projects[metarepo] = ""
+
+				for project := range story.Projects {
+					color.Green(project)
+					// Get the open pull request
+					openPullRequest, err := getOpenPullRequest(client, ctx, story, project)
+					if err != nil {
+						switch err.Error() {
+						// If there is no open pull request
+						case ErrCouldNotFindOpenPullRequest(story.Name).Error():
+							// Get the closed pull request
+							closedPullRequest, err := getClosedPullRequest(client, ctx, story, project)
+							if err != nil {
+								switch err.Error() {
+								// If there is no closed pull request, a pull request was never opened
+								case ErrCouldNotFindClosedPullRequest(story.Name).Error():
+									fmt.Printf("could not find an open or closed pull request for %s\n", story.Name)
+									continue
+								// Something else went wrong here with the call to list closed pull requests
+								default:
+									fmt.Println(err)
+									continue
+								}
+							}
+
+							// Report that the pull request has already been closed with a link
+							fmt.Println("pull request has already been closed")
+							fmt.Println(*closedPullRequest.HTMLURL)
+							continue
+						// Something else went wrong here with the call to list open pull requests
+						default:
+							return err
+						}
+					}
+
+					_, resp, err := client.PullRequests.Merge(ctx, story.Orgranisation, project, *openPullRequest.Number, "", &github.PullRequestOptions{
+						CommitTitle: messages[0],
+						MergeMethod: "squash",
+						SHA:         story.Hashes[project],
+					})
+
+					if err != nil {
+						return err
+					}
+
+					switch resp.StatusCode {
+					case http.StatusOK:
+						fmt.Println("pull request successfully merged")
+					case http.StatusMethodNotAllowed:
+						fmt.Println("pull request is not mergeable")
+					case http.StatusConflict:
+						fmt.Println("head branch was modified, review and try the merge again")
+					}
+
+					fmt.Println(*openPullRequest.HTMLURL)
+					time.Sleep(1 * time.Second)
+				}
+
+				return nil
+			}
 
 			// Checkout master and merge story in all projects
 			for project := range story.Projects {
